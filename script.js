1e02dececb592ed36bd321e3244c8ab4930ea105
shahfawadkhan <you@sfawadkhan92@gmail.com>
2024-09-23 07:18:51 -0700

    update the function where empty list will not be added

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-var addTodo = document.querySelector("#add-todo");
-var listItem = document.querySelector('.list-items');
+let addTodo = document.querySelector("#add-todo");
+let listItem = document.querySelector('.list-items');
+let inputField = document.querySelector("#input-text");
 
 addTodo.addEventListener('click', addTask);
 
 function addTask() {
-    var inputText = document.querySelector("#input-text").value; 
-    var li = document.createElement('li');
+    let inputText = inputField.value.trim(); 
+
+    if(inputText){
+    let li = document.createElement('li');
     li.textContent = inputText;
     listItem.appendChild(li);
+    inputField.value ='';
+    }
+    else{
+        alert('add task')
+    }
+    
 }
