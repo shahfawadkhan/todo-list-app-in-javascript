c6cfada57901c9d84f8b522c80aa9b6f7834f7a2
shahfawadkhan <you@sfawadkhan92@gmail.com>
2024-09-23 07:08:20 -0700

    update the function where li item will be created if someone click on the btn and also append it to the ul

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var addTodo = document.querySelector("#add-todo");
-var listItem = document.querySelector('.list-items')
+var listItem = document.querySelector('.list-items');
 
-addTodo.addEventListener('click' , addTask);
+addTodo.addEventListener('click', addTask);
 
-function addTask(){
+function addTask() {
+    var inputText = document.querySelector("#input-text").value; 
     var li = document.createElement('li');
+    li.textContent = inputText;
     listItem.appendChild(li);
 }
