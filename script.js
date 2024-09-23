e8c4a6bc65f0a832734d1751997e2547947661f2
shahfawadkhan <you@sfawadkhan92@gmail.com>
2024-09-23 07:02:25 -0700

    make a functioin for add todo btn

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// var addTodo = document.querySelector("#add-todo");
+var addTodo = document.querySelector("#add-todo");
+var listItem = document.querySelector('.list-items')
+
+addTodo.addEventListener('click' , addTask);
+
+function addTask(){
+    var li = document.createElement('li');
+    listItem.appendChild(li);
+}
